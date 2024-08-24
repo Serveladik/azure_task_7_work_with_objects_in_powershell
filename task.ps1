0593cd68d8ead41b7d383d9fcc467b9aebbd27f6
Serveladus <serveladik@gmail.com>
2024-08-24 16:37:57 +0300

    Solution

--- task.ps1
+++ task.ps1
@@ -1,1 +1,17 @@
-# Write your code here
+$PathData = Get-ChildItem ./data
+$ResultList = @()
+
+foreach ($file in $PathData)
+{
+    $content = Get-Content $file.FullName | ConvertFrom-Json
+    foreach ($data in $content)
+    {
+        if ($data.Name -eq "Standard_B2pts_v2")
+        {
+            $ResultList += $file.BaseName
+        }
+    }
+    $ResultList | ConvertTo-Json | Out-File -FilePath ./result.json -Encoding utf8
+    $ResultList | ForEach-Object { Write-Host $_}
+}
+
